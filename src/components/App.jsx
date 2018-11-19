7bebd374f4e998436d3e52705189843394c47741
Nevnet99 <luke-brannagan@hotmail.com>
2018-11-19 21:11:40 +0000

    Stopped the app rendering the topnav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import '../styles/app.scss';
-import TopNav from '../components/TopNav';
 
 const App = () => (
   <div>
-    <TopNav />
+    <h1>Hello</h1>
   </div>
 );
 
